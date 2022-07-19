a448f69e7fea4c7fd689c351f4b4ce8b6cf6075f
0xKoichi <94922954+0xKoichi@users.noreply.github.com>
2022-07-19 14:41:55 +0100

    cleanup

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -17,21 +17,12 @@ public class Block : MonoBehaviour
     public int maxNodes = 4;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
         w = (int)sprite.rect.width;
         h = (int)sprite.rect.height;
-        var pixelCount = w * h;
-        var offset = pixelCount / 4;
-
         GetAllNodes();
-        
-        // Color[] pixels = sprite.texture.GetPixels(0);
-        // pixelSections = SplitPixelArray(pixels, offset);
-        // if (pixelSections.Count > 0) GetNodes();
-        
     }
 
     /// <summary>
@@ -53,35 +44,10 @@ public class Block : MonoBehaviour
     /// <summary>
     /// Ensures that the current block can be divided by the user
     /// defined minimum node count. Assumes that the block is a
-    /// square with even width and height and is power of two,
-    /// i.e., 8x8, 16x16, 32x32, 64x64, 128x128...
+    /// square is power of two, e.g., 8x8, 8x16, 16x16, 16x32, 32x32 etc...
     /// </summary>
     void GetAllNodes()
     {
-
-        
-        // int? potentialMinNode = GetLowestMinNode();
-        // bool blockValid = (potentialMinNode != null);
-        // if (!blockValid) Debug.LogError($"Block size not valid!\n(Block Width: {w}, Block Height: {h}");
-
-        
-        // bool minNodeValid = (minNodes >= potentialMinNode && minNodes % potentialMinNode == 0);
-
-        // if (minNodeValid)
-        // {
-            
-        //     var minPixelDensityY = (int)(Math.Pow(h, 2) / minNode);
-
-        //     var width = maxSlicesWidth * 2;
-        //     var height = maxSlicesHeight * 2;
-            
-        //     for (var i = 0; i < minNodes; i++)
-        //     {
-        //         var x = 0;
-        //         var y = 0;
-        //     }
-        // 
-
         if(ValidateMaxNodes(maxNodes))
         {
             var nHeight = (h / (maxNodes / 2));
@@ -90,30 +56,33 @@ public class Block : MonoBehaviour
             var mY = h;
             var mX = w;
 
-
-
             // Get other nodes.
             for (var i = 0; i < mX; i+=nWidth)
             {
                 for (var j = 0; j < mY; j+=nHeight)
                 { 
-                    // var position = new Rect(i, j, nWidth, nHeight);
                     var texture = GetNode(i, j, nWidth, nHeight);
                     texture.name = $"{gameObject.name} ({i}, {j})";
-                    // GameObject go = new GameObject($"{i} {j}", typeof(SpriteRenderer));
-                    // var sprite = Sprite.Create(texture, position);
-                    // Instantiate(go);
-                    
                 }
             }
         }
     }
 
+    /// <summary>
+    /// Get the pixels from the sprite's texture at the given coordinates, create a new texture from
+    /// those pixels, and save the new texture to disk
+    /// </summary>
+    /// <param name="x">The x coordinate of the top left corner of the node.</param>
+    /// <param name="y">The y coordinate of the top-left corner of the node.</param>
+    /// <param name="w">width of the node</param>
+    /// <param name="h">height of the node</param>
+    /// <returns>
+    /// A Texture2D object.
+    /// </returns>
     Texture2D GetNode(int x, int y, int w, int h)
     {
         var mipLevel = 0;
         var destMipLevel = 0;
-
         var nodePixels = sprite.texture.GetPixels(x, y, w, h, mipLevel);
         var node = new Texture2D(w, h);
         node.SetPixels(nodePixels, destMipLevel);
@@ -124,41 +93,12 @@ public class Block : MonoBehaviour
         return node;
     }
 
+    
     void SaveTexture(Texture2D data, string fileName)
     {
         byte[] bytes = ImageConversion.EncodeArrayToPNG(data.GetRawTextureData(), data.graphicsFormat, (uint)data.width, (uint)data.height);
         File.WriteAllBytes($"{Application.dataPath}/{fileName}.png", bytes);
     }
-
-    bool isDivisible (int p)
-    {
-        var sqrt = Mathf.Sqrt(p);
-        return (sqrt % 4 == 0);
-    }
-
-    // List<Color[]> SplitPixelArray (Color[] pixels, int offset)
-    // {
-    //     List<Color[]> result = new List<Color[]>();
-    //     for (var i = 0; i < pixels.Length; i+=offset)
-    //     {
-    //         Color[] currentPixels = new Color[offset];
-    //         Array.Copy(pixels, i, currentPixels, 0, offset);
-    //         result.Add(currentPixels);
-    //     }
-    //     Debug.Log(result.Count());
-    //     return result;
-    // }
-
-    // void GetNodes()
-    // {
-    //    foreach (var section in pixelSections)
-    //    {
-    //         var node = new Texture2D(w / 4, h / 4);
-    //         node.SetPixels(section);
-    //         node.Apply();
-    //         nodes.Add(node);
-    //    }
-    // }
 
 
 
